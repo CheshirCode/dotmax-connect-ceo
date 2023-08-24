9bee64fb851738f38b07439c1c252d4dd5b90c19
CheshireCode <50378606+CheshirCode@users.noreply.github.com>
2023-08-24 12:34:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.querySelector("#searchCard").oninput = function () {
+  let val = this.value.trim();
+
+  let searchItems = document.querySelectorAll(".searchCard .phone__card");
+  if (val != "") {
+    searchItems.forEach(function (elem) {
+      if (elem.innerText.search(val) == -1) {
+        elem.classList.add("hide");
+      } else {
+        elem.classList.remove("hide");
+      }
+    });
+  } else {
+    searchItems.forEach(function (elem) {
+      elem.classList.remove("hide");
+    });
+  }
+};
